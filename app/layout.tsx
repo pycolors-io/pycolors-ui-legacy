e6850fb474342f7a33cbb71ec4b51d599f976ec3
Py Colors <patweb44@gmail.com>
2026-01-11 15:41:19 +0100

    Adds Google Tag Manager integration
    Integrates Google Tag Manager via Next.js Script to enable
    analytics tracking across the application. Facilitates better
    insight into user behavior and marketing effectiveness.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
 
+import './global.css';
 import { inter } from './fonts';
 
 export const metadata: Metadata = {
@@ -40,13 +41,34 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
       lang="en"
       suppressHydrationWarning
       className={inter.className}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NQNBGFLM');
+            `,
+          }}
+        />
+      </head>
+
       <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
       </body>
