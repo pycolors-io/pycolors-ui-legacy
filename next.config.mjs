75c7c27da15c6eead538b5158a36100af3ca5713
P@trice <patweb44@gmail.com>
2026-02-18 16:47:02 +0100

    chore: add redirects to external URL in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://pycolors.io/:path*',
+        permanent: true, // 308
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
